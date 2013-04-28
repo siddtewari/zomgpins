71ca2ee7742742c9cb08d8ccb90268529c04bf38
siddtewari <sidtewari@gmail.com>
2013-04-28 13:41:21 -0400

    Initial Commit

---
+++ READM.md
@@ -0,0 +1,6 @@
+# zomgpins - Project Pinterest 
+
+This is a practice application for 
+[*ZOMG Rails*](http://siddtewari.com)
+
+by [Sidd Tewari](http://siddtewari.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zomgpins::Application.config.secret_token = 'b03813e8ffe98453429903de8a2654ec5f0002ed843cd47e1ac349a8057eb667f183936274e117f74945f476839c09326463699e5b7607abae9d586f23c6db2e'
