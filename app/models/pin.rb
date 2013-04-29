d137c91cf79580df17efec434b01805e4a1edcca
siddtewari <sidtewari@gmail.com>
2013-04-29 07:25:43 -0400

    Add Pins Scaffold

---
+++ app/models/pin.rb
@@ -0,0 +1,4 @@
+class Pin < ActiveRecord::Base
+  attr_accessible :description
+  validates :description, presence: true, length: {minimum: 2, maximum: 300}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Zomgpins::Application.routes.draw do
+  resources :pins
+
+
   devise_for :users
   root :to => 'pages#home'
   get 'about' => 'pages#about'
